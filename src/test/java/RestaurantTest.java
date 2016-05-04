1e15a19657e0b1eb36dadd1d58a21364b79874b7
farnoosh johnson, Fernanda Lowe and LaTaevia Berry <fernanda@>
2016-05-04 16:20:22 -0700

    add passing tests, working on creating saving cuisine to DB while snacking on Persian pistascios

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -22,13 +22,51 @@ public class RestaurantTest {
 
   @Test
   public void restaurant_instantiatesCorrectly_true() {
-    Restaurant myRestaurant = new Restaurant("Olive Garden");
+    Restaurant myRestaurant = new Restaurant("Olive Garden", 1);
     assertTrue(myRestaurant instanceof Restaurant);
   }
   @Test
   public void restaurant_instantiatesWithName_String() {
-    Restaurant myRestaurant = new Restaurant("Olive Garden");
+    Restaurant myRestaurant = new Restaurant("Olive Garden", 1);
     assertEquals("Olive Garden", myRestaurant.getName());
+  }
+  @Test
+  public void restaurant_getMultiplySameRestaurantName_true() {
+    Restaurant firstRestaurant = new Restaurant("Olive Garden", 1);
+    Restaurant secondRestaurant = new Restaurant("Olive Garden", 1);
+    assertTrue(firstRestaurant.equals(secondRestaurant));
+  }
+
+  @Test
+  public void restaurant_getRestaurantandSaveIt_true() {
+    Restaurant myRestaurant = new Restaurant("Olive Garden", 1);
+    myRestaurant.save();
+    assertTrue(Restaurant.all().get(0).equals(myRestaurant));
+  }
+  @Test
+  public void restaurant_getRestaurantId_int() {
+    Restaurant myRestaurant = new Restaurant("Olive Garden", 1);
+    myRestaurant.save();
+    Restaurant savedRestaurant = Restaurant.all().get(0);
+    assertEquals(myRestaurant.getId(), savedRestaurant.getId());
+  }
+
+  @Test
+  public void find_findRestaurantInDatabase_true() {
+    Restaurant myRestaurant = new Restaurant("Olive Garden", 1);
+    myRestaurant.save();
+    Restaurant savedRestaurant = Restaurant.find(myRestaurant.getId());
+    assertTrue(myRestaurant.equals(savedRestaurant));
+  }
+
+  @Test
+  public void save_saveCuisineIdIntoDB_true() {
+    Cuisine myCuisine = new Cuisine ("Italian");
+    myCuisine.save();
+    Restaurant myRestaurant = new Restaurant ("Olive Garden", myCuisine.getId());
+    myRestaurant.save();
+    Restaurant savedRestaurant = Restaurant.find(myRestaurant.getId());
+    assertEquals(savedRestaurant.getCuisineId(), myCuisine.getId());
   }
 
 }
