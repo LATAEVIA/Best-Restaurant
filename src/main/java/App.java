95e1eff868c5e4b6c193673757e230e9c2585084
Farnoosh Johnson, Fernanda Lowe and LaTaevia Berry <lataevia@>
2016-05-05 14:52:44 -0700

    add working project with limited testing, will continue testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -21,6 +20,53 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("cuisines", Cuisine.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/cuisines", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String cuisineName = request.queryParams("cuisine-name");
+      Cuisine newCuisine = new Cuisine(cuisineName);
+      newCuisine.save();
+      response.redirect("/");
+      return null;
+    });
+
+    get("/cuisines/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Cuisine existingCuisine = Cuisine.find(Integer.parseInt(request.params(":id")));
+      model.put("cuisine", existingCuisine);
+      model.put("restaurants", existingCuisine.getRestaurants());
+      model.put("template", "templates/restaurants.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/restaurants", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String restaurantName = request.queryParams("restaurant-name"); //olive garden
+      Cuisine existingCuisine = Cuisine.find(Integer.parseInt(request.queryParams("cuisineId")));
+      Restaurant newRestaurant = new Restaurant(restaurantName,existingCuisine.getId());
+      System.out.println(newRestaurant.getCuisineId());
+      newRestaurant.save();
+      String url = String.format("/cuisines/%d", existingCuisine.getId());
+      response.redirect(url);
+      // model.put("cuisine",existingCuisine);
+      // model.put("template", "templates/restaurant-Added.vtl");
+      return null;
+    });
+
+
+    // get("/restaurants", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //   Cuisine newCuisine = Cuisine.find(Integer.parseInt(request.params(":id")));
+    //   model.put("restaurants", Restaurant.all());
+    //   model.put("template", "templates/restaurants.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
 
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
-// import org.sql2o.*; // for DB support
+import org.sql2o.*; // for DB support
 import org.junit.*; // for @Before and @After
 
 public class AppTest extends FluentTest {
@@ -17,8 +17,46 @@ public class AppTest extends FluentTest {
     return webDriver;
   }
 
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Test
+    public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Best Restaurants App");
+      assertThat(pageSource()).contains("Cuisine");
+      assertThat(pageSource()).contains("favorite cuisine");
+    }
+
+  @Test
+    public void getCuisineReturnSamePage() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Best Restaurants App");
+      fill("#cuisine-name").with("Italian");
+      submit(".btn");
+      assertThat(pageSource()).contains("Italian");
+    }
+
+  @Test
+    public void getCuisineIdReturnRestaurantAppend() {
+      goTo("http://localhost:4567/");
+      fill("#cuisine-name").with("Italian");
+      submit(".btn");
+      click("a", withText("Italian"));
+      assertThat(pageSource()).contains("List");
+    }
+
+  @Test
+    public void getRestaurantReturnWithCuisineId() {
+      goTo("http://localhost:4567/");
+      fill("#cuisine-name").with("Italian");
+      submit(".btn");
+      click("a", withText("Italian"));
+      assertThat(pageSource()).contains("List of all restaurants with Italian cuisine");
+      fill("#restaurant-name").with("Olive garden");
+    }
 
 }
